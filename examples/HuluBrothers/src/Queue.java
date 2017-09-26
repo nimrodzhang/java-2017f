17066a8707e2e3faca767aed4844053a5b07e230
Chun Cao <caochun@gmail.com>
2017-09-26 13:46:49 +0800

    update

--- examples/HuluBrothers/src/Queue.java
+++ examples/HuluBrothers/src/Queue.java
@@ -2,29 +2,55 @@ public class Queue {
 
     public Position[] positions;
 
-    public Queue(){
-        for (int i=0;i <positions.length; i++){
-            positions[i]= new Position();
+    public Queue() {
+        positions = new Position[0];
+        for (int i = 0; i < positions.length; i++) {
+            positions[i] = new Position();
             positions[i].index = i;
             positions[i].holder = null;
         }
     }
 
-    public void enqueue(Object[] objects){
+    public void enqueue(Huluwa[] huluwas) {
 
-        if (objects.length > this.positions.length){
+        if (huluwas.length > this.positions.length) {
             return; //actually should throw exception
         }
 
-        positions[0].holder = objects[1];
-        //...
+        positions[1].holder = huluwas[0];
+        positions[2].holder = huluwas[2];
+        positions[3].holder = huluwas[6];
+        positions[4].holder = huluwas[4];
+        positions[5].holder = huluwas[3];
+        positions[6].holder = huluwas[5];
+        positions[7].holder = huluwas[5];
 
+        //留个空位
+        positions[0].holder = null;
+
+        //...
 
     }
 
+    public void sort() {
+
+        Huluwa temp;
+
+        int j;
+        for (int i = 1; i < positions.length; i++) {
+            Huluwa huluwa = positions[i].holder;
+            j = i - 1;
+            //如果huluwa小于后端数，那后端的数要顺移
+            while (j >= 0 && huluwa.seniority < positions[j].holder.seniority) {
+                positions[j + 1].holder = positions[j--].holder;
+            }
+            //将huluwa插入到指定位置
+            positions[j + 1].holder = huluwa;
+        }
+    }
 }
 
-class Position{
+class Position {
     public int index;
-    public Object holder;
+    public Huluwa holder;
 }
